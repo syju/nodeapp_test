118fc7da369df49dbe19f9f605c0e2f575a2848f
syju <32048847+syju@users.noreply.github.com>
2021-12-15 00:28:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "CICD DEPLOYMENT" }');
+    res.send('{ "response": "CICD k8s DEPLOYMENT" }');
 });
 
 app.get('/will', function (req, res) {
